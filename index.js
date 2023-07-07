b2aaf1280ac873769f8352309c1266ad2fd50f2b
kabuke <kabuke13@gmail.com>
2023-07-07 13:01:44 +0100

    More efficient changes

--- index.js
+++ index.js
@@ -20,19 +20,11 @@ function distanceTravelledInFeet(pickUp, dropOff) {
 
 
 function calculatesFarePrice(start, destination) {
-    const destStart = ((destination - start) * 264)
-    const startDest = ((start - destination) * 264)
-    if (destStart >= 0 && destStart < 400) {
+    if (distanceTravelledInFeet(start, destination) >= 0 && distanceTravelledInFeet(start, destination) <= 400) {
         return 0
-    } else if (startDest >= 0 && startDest <400) {
-        return 0
-    } else if (destStart > 400 && destStart <= 2000) {
-        return (destStart - 400) * 0.02
-    } else if (startDest > 400 && startDest <= 2000) {
-        return (startDest - 400) * 0.02
-    } else if (destStart > 2000 && destStart <= 2500) {
-        return 25
-    } else if (startDest > 2000 && startDest <= 2500) {
+    } else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * 0.02
+    } else if (distanceTravelledInFeet(start, destination) >2000 && distanceTravelledInFeet(start, destination) <= 2500) {
         return 25
     } else {
         return "cannot travel that far"
